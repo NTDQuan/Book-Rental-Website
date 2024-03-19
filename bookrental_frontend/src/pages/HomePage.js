96a7c3e7fe162180ff35622d5a96629cc85659c6
Dinh Quan <ntdquan2003@gmail.com>
2024-03-19 09:44:31 +0700

    Feat: Add Cover Image to front page

--- bookrental_frontend/src/pages/HomePage.js
+++ bookrental_frontend/src/pages/HomePage.js
@@ -1,7 +1,23 @@
+import CoverImage from "../image/home_cover.jpg"
+import "./style/HomePage.css"
 
 function HomePage() {
     return(
-        <h1>Home</h1>
+        <div className="Wrapper">
+            <div className="Home_Cover_Container">
+                <div className="Home_Cover_Image">
+                    <img src={CoverImage} alt="home_cover"/>
+                </div>
+                <div className="Home_Cover_Overlay">
+                    
+                </div>
+                <div className="Home_Cover_Text">
+                    <a>Welcome to Rent A Book !</a>
+                </div>
+            </div>
+            
+        </div>
+
     );
 }
 
